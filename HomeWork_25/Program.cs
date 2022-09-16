f03036b20b407a83bce00205a41bc2e5cb5944de
Wiktor <viktor.tomin.@mail.ru>
2022-09-16 16:28:44 +0500

    HomeWork 25

---
+++ HomeWork_25/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) 
+// и возводит число A в натуральную степень B. Решить задачу с использованием методов.
+
+
+int a = ReadInt("Input a: ");
+int b = ReadInt("Input b: ");
+ GetDegree( a, b);
+
+void GetDegree(int a, int b)
+{
+    int result = 1;
+    for (int i = 1; i <= b; i++)
+    {
+        result = result * a;
+    }
+    Console.WriteLine(result);
+}
+
+int ReadInt(string message)
+{
+    Console.WriteLine(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+
+
+
+
+

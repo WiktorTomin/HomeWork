da301b1366c61f447d21d7afe1623dd7a043ff26
Wiktor <viktor.tomin.@mail.ru>
2022-09-16 16:23:25 +0500

    HomeWork 27

---
+++ HomeWork_27/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+
+// Подсчет суммы цифр в числе.
+
+int SumDigits(int value)
+{
+    int sum = 0;
+    while(value > 0)
+    {
+        sum += value % 10;
+        value /= 10;
+    }
+     return sum;
+}
+
+Console.Write("Input number: ");
+int mynumb = int.Parse(Console.ReadLine());
+
+int result = SumDigits(mynumb);
+Console.WriteLine(result);
+
+

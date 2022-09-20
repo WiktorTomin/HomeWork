63bd7d8050ebc669e0b667a9a39b9d377141c451
Wiktor <viktor.tomin.@mail.ru>
2022-09-20 21:29:15 +0500

    HomeWork 37

---
+++ HomeWork_37/Program.cs
@@ -0,0 +1,57 @@
+﻿// Задача 37: Найдите произведение пар чисел в одномерном массиве. 
+// Парой считаем первый и последний элемент, второй и предпоследний и т.д.
+// Результат запишите в новом массиве.
+
+
+
+int[] MultPairNumbers(int[] array)
+{
+  int size = 0;
+  int[] result = null;
+
+    if (array.Length / 2 == 0 )
+    {
+        size = array.Length / 2;
+        result = new int[size];
+    
+        for (int i = 0; i < size; i++)
+        {
+            result[i] = array[i] * array[array.Length - i -1];
+        }
+    }
+    else
+    {
+        size = (array.Length / 2) + 1; 
+        result = new int[size];
+    
+        for(int i = 0; i < size; i++)
+        {
+            if (i == (size - 1))
+            {
+                result[i] = array[i];
+            }
+
+            else
+            {
+                result[i] = array[i] * array [array.Length - i - 1];
+            }
+        
+        }
+    
+    }
+        return result;
+}
+
+int[] array = new int[] {1, 4, 6, 3, 7};
+int[] result = MultPairNumbers (array);
+
+Console.WriteLine($"Результат произведения пар чисел в массиве: {result}");
+
+
+
+
+
+
+
+
+

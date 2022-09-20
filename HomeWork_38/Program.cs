d370c555f634ffae8b2a819d52a581c75a9dfbdf
Wiktor <viktor.tomin.@mail.ru>
2022-09-20 21:31:22 +0500

    HomeWork 38

---
+++ HomeWork_38/Program.cs
@@ -0,0 +1,35 @@
+﻿// Задача 38: Задайте массив вещественных чисел.
+// Найдите разницу между максимальным и минимальным элементом массива.
+
+int[] RealNumberSize = new int[5];
+  for (int i = 0; i < RealNumberSize.Length; i++ )
+  {
+    RealNumberSize[i] = new Random().Next(1, 100);
+    Console.Write(RealNumberSize[i] + " ");
+  }
+
+int maxValue = RealNumberSize[0];
+int minValue = RealNumberSize[0];
+
+  for (int i = 1; i < RealNumberSize.Length; i++)
+  {
+    if (maxValue < RealNumberSize[i])
+    {
+      maxValue = RealNumberSize[i];
+    }
+    
+    if (minValue > RealNumberSize[i])
+    {
+      minValue = RealNumberSize[i];
+    }
+  }
+
+  int result = maxValue - minValue;
+
+  Console.WriteLine($"Разность между ({maxValue}) и ({minValue}) элементами равна: {result}");
+
+
+
+
+
+

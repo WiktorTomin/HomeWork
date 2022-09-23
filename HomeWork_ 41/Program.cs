9f0b080b595d0bed4f2296e435b5a8d818446bb3
Wiktor <viktor.tomin.@mail.ru>
2022-09-23 17:51:19 +0500

    HomeWork 41

---
+++ HomeWork_ 41/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел.
+// Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+Console.Write("Введите числа (через пробел): ");
+
+int[] numbers = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+int result = 0;
+ 
+for (int i = 0; i < numbers.Length; i++)
+{
+    if (numbers[i] > 0)
+    {
+        result = result + 1;
+    }
+}
+ Console.WriteLine($"Количество положительных чисел: {result}");
+
+
+
+
+

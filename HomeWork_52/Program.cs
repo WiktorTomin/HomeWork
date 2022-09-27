c4503cd9eeaa97dbc5ed817a29c38b5eee20b771
Wiktor <viktor.tomin.@mail.ru>
2022-09-27 15:41:37 +0500

    HomeWork 52

---
+++ HomeWork_52/Program.cs
@@ -0,0 +1,48 @@
+﻿// Задача 52. Задайте двумерный массив из целых чисел. 
+// Найдите среднее арифметическое элементов в каждом столбце.
+
+double[] GetColumnSum(int[,] array)
+{
+    double[] avgSumArray = new double[array.GetLength(1)];
+    
+    for(int i = 0; i < array.GetLength(1); i++)
+    {
+       for(int j = 0; j < array.GetLength(0); j++)
+        {
+            avgSumArray[i] =  avgSumArray[i] + array[j, i];
+        }
+        avgSumArray[i] =  avgSumArray[i] / array.GetLength(0);
+    }
+        return avgSumArray;
+}
+
+int[,] array = new int[5, 7];
+
+for(int i = 0; i < array.GetLength(0); i++)
+    {
+       for(int j = 0; j < array.GetLength(1); j++)
+        {
+           array[i, j] = new Random().Next(1, 4);
+        }
+    }
+
+double[] result  = GetColumnSum(array);
+ 
+Console.ReadKey();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
